331f8682fa58a330a35f2a72592a5d3f35ce83de
Ayushi <awadhwa01.ayu@gmail.com>
2018-08-10 00:49:15 -0400

    Completed Lazyloading

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,6 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader{
     constructor(){
+        this.lazyImages = $(".lazyload");
         this.siteHeader = $(".site-header");
         this.headerTriggerElement = $(".large-hero__title");
         this.headerLinks = $(".primary-nav a");
@@ -11,6 +12,13 @@ class StickyHeader{
         this.pageSections = $(".page-section");
         this.createPageSectionWaypoints();
         this.addSmoothScrolling();
+        this.refreshWaypoints();
+    }
+
+    refreshWaypoints(){
+        this.lazyImages.on('load', function(){
+            Waypoint.refreshAll();
+        })
     }
 
     addSmoothScrolling(){
